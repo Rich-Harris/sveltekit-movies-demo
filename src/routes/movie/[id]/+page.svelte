6fdcf1a8a3add4618473844fa349e29e70030b69
Rich Harris <rich.harris@vercel.com>
2024-03-07 22:24:30 -0700

    styles

--- src/routes/movie/[id]/+page.svelte
+++ src/routes/movie/[id]/+page.svelte
@@ -4,17 +4,27 @@
 
 <div>
 	<img src={data.movie.thumbnail} alt="movie poster" />
-	<h1 style="margin-top:0">{data.movie.title} ({data.movie.year})</h1>
+	<h1>{data.movie.title} ({data.movie.year})</h1>
 	<p>{data.movie.extract}</p>
 </div>
 
 <style>
+	h1 {
+		margin-top: 0;
+		text-wrap: balance;
+		line-height: 1.2;
+	}
+
 	img {
 		background: #eee;
 		margin: 0 0 1rem 1rem;
 		aspect-ratio: 2 / 3;
-		width: 40%;
+		width: 100%;
 		float: right;
 		object-fit: cover;
+
+		@media (min-width: 24rem) {
+			width: 40%;
+		}
 	}
 </style>
